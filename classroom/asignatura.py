8fdeeddb8055eda0c8e37c5207c07f854a6166ae
MSRestreLeon <monicarestrepoleon@gmail.com>
2023-03-07 01:10:42 -0500

    lolz

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -3,6 +3,9 @@ class Asignatura:
     def __init__(self, nombre, salon):
         self._nombre = nombre
         self._salon = salon
+        nombre = ""
+        salon = "remoto"
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+        linea = ("Grupo de estudiantes" +self._grupo)
+        return linea
